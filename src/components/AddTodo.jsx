40cf75921650d2c55946e28e8b8972c079c6b94d
Iman <imankhorshidi72@gmail.com>
2022-07-03 21:41:01 +0430

    Fix: AddTodo Component

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AddTodo = ({ todos, setTodos }) => {
   const [todo, setTodo] = useState("");
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
 
   // Function for submit and add todo
   const submitHandler = (e) => {
@@ -26,6 +32,7 @@ const AddTodo = ({ todos, setTodos }) => {
         className="w-[75%] text-xl py-2 px-4 border border-gray-500 rounded-xl focus:outline-gray-700 md:w-[50%]"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
+        ref={ref}
       />
       <button className="text-4xl text-white flex items-center justify-center bg-green-700 py-1 px-4 rounded-xl transition-all duration-300 hover:bg-green-800">
         +

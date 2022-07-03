c6b75c5d75fe113dbaf683256ec32785f4b661b2
Iman <imankhorshidi72@gmail.com>
2022-07-04 01:12:59 +0430

    Fix: TodosBox Component

--- src/components/TodosBox.jsx
+++ src/components/TodosBox.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+
 const TodosBox = ({ todos, setTodos }) => {
+  const [boxTodos, setBoxTodos] = useState([]);
+
+  useEffect(() => {
+    setBoxTodos(JSON.stringify(todos));
+  }, [todos]);
+
+  const handleEnterClick = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      setTodos(JSON.parse(boxTodos));
+    }
+  };
+
   return (
     <div className="w-full flex justify-center">
       {todos.length > 0 && (
         <textarea
           className="rounded-xl resize-none w-[90%] h-[250px] p-2 text-xl md:w-[55%]"
-          value={JSON.stringify(todos)}
+          value={boxTodos}
+          onChange={(e) => setBoxTodos(e.target.value)}
+          onKeyDown={handleEnterClick}
         ></textarea>
       )}
     </div>

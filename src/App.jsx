4a4918da343c92eab81690db392e22338d63f5aa
Iman <imankhorshidi72@gmail.com>
2022-07-03 12:31:24 +0430

    Add: TodoList Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BsFillFileCheckFill } from "react-icons/bs";
+
 const App = () => {
   return (
     <>
-      <div className="text-center p-2">
-        <h1 className="text-5xl">My Todos</h1>
+      <div className="p-2">
+        <div className="text-center p-2 flex items-center justify-center gap-4">
+          <h1 className="text-5xl">My Todos</h1>
+          <BsFillFileCheckFill className="text-5xl text-green-700" />
+        </div>
       </div>
     </>
   );

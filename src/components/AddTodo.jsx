c57e46bcf4b9c84b44fa6e94ab04d4d1bce448eb
Iman <imankhorshidi72@gmail.com>
2022-07-03 13:36:45 +0430

    Fix: AddTodo component

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -6,7 +6,7 @@ const AddTodo = () => {
         placeholder="Add todo"
         className="w-[50%] py-3 px-4 border border-gray-500 rounded-xl focus:outline-gray-700"
       />
-      <button className="text-4xl text-white flex items-center justify-center bg-green-700 py-1 px-4 rounded-xl transition-all duration-300  hover:bg-green-800">
+      <button className="text-4xl text-white flex items-center justify-center bg-green-700 py-1 px-4 rounded-xl transition-all duration-300 hover:bg-green-800">
         +
       </button>
     </div>
